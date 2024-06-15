5492fe6d10c9ab65bb3c5b5a70a6f3a2d1bdb713
Roman Shaparev <shaparev.roman@gmail.com>
2024-06-15 17:26:48 +0700

    added live reloading

--- microservice/main.go
+++ microservice/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
 
 func main() {
-    fmt.Println("hello world")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World"))
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
